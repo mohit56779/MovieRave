6a01f552c7a528f64ae3fbbee2065f669e1b4897
Mohit <mohit56779@gmail.com>
2021-06-20 00:43:33 -0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-app.get('/', (req,res) =>{
-    res.send('hello world');
-});
+const {User} = require('./models/user');
+
+mongoose.connect('mongodb+srv://mohit56779:mohit1234@movierave.cbrl8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+                    {useNewUrlParser:true}).then(() =>console.log('DB connected.'))
+                                            .catch(err => consolde.error(err));
 
 
-app.listen(5000);
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+app.post('/api/users/register', (req,res) => {
+    const user = new User(req.body);
+
+    user.save( (err,userData) => {
+        if(err) return res.json({success:false, err})
+    })
+
+    return res.status(200).json({
+        success:true
+    })
+})
+
+app.listen(4200);
